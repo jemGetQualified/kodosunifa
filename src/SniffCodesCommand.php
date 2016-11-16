307704945ef4db9fcd914b48a01e25058b09296a
Jem Raymundo <jeremuel.raymundo@gqaustralia.edu.au>
2016-11-16 23:02:08 +0000

    Update Composer

--- src/SniffCodesCommand.php
+++ src/SniffCodesCommand.php
@@ -5,6 +5,8 @@ namespace Kodosunifa;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Process;
 
 class SniffCodesCommand extends Command
 {
@@ -14,8 +16,19 @@ class SniffCodesCommand extends Command
             ->setDescription('Sniff code into psr-2 based');
     }
 
+
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $output->writeln('Sniffing Codes...');
+
+        $process = new Process('phpcs -n --standard=PSR2 app/Http/Controllers/');
+        $process->run();
+
+        if (!$process->isSuccessful()) {
+            throw new ProcessFailedException($process);
+        }
+
+        echo $process->getOutput();
 
     }
 }
