bbd7b9104299b85bb095d17e99e2b92010cd1c2d
Jem Raymundo <jeremuel.raymundo@gqaustralia.edu.au>
2016-11-17 05:06:16 +0000

    Added codes

--- kodosunifa.php
+++ kodosunifa.php
@@ -3,12 +3,9 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
-
 use Kodosunifa\SniffCodesCommand;
 use Symfony\Component\Console\Application;
 
 $application = new Application();
-
 $application->add(new SniffCodesCommand());
-
 $application->run();
--- src/SniffCodesCommand.php
+++ src/SniffCodesCommand.php
@@ -5,30 +5,52 @@ namespace Kodosunifa;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Process\Exception\ProcessFailedException;
 use Symfony\Component\Process\Process;
 
 class SniffCodesCommand extends Command
 {
     protected function configure()
     {
-        $this->setName('kodosunifa:sniff-code')
-            ->setDescription('Sniff code into psr-2 based');
+        $this->setName('kodosunifa:sniff-code')->setDescription('Sniff code into psr-2 based');
     }
 
-
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     *
+     * @return void
+     */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $output->writeln('Sniffing Codes...');
 
-        $process = new Process('phpcs -n --standard=PSR2 app/Http/Controllers/');
+        $process = new Process('phpcs -n --standard=PSR2 --ignore=*/tests/*,*/data/*,*/storage/*,*/test/*,*/vendor/* app/ bootstrap/ config, routes');
         $process->run();
 
-        if (!$process->isSuccessful()) {
-            throw new ProcessFailedException($process);
+        if ($process->getOutput()) {
+            $this->handleErrorMessages($process->getOutput(), $output);
+            exit(1);
         }
 
-        echo $process->getOutput();
+        $this->handleSuccessMessages($output);
+        exit(0);
+    }
 
+    /**
+     * @param string $message
+     * @param OutputInterface $output
+     */
+    private function handleErrorMessages($message, OutputInterface $output)
+    {
+        $output->writeln($message);
+        $output->writeln('<error>STYLE CHECK FOUND ERRORS PLEASE FIX BEFORE PROCEEDING!</error>');
+    }
+
+    /**
+     * @param OutputInterface $output
+     */
+    private function handleSuccessMessages(OutputInterface $output)
+    {
+        $output->writeln('<info>PSR-2 STYLE CHECK PASSED</info>');
     }
 }
