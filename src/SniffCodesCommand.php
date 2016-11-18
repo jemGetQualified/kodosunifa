108bba69e19358c63f343416a83d2c7e576a40ef
Jem Raymundo <jeremuel.raymundo@gqaustralia.edu.au>
2016-11-18 02:55:03 +0000

    finalized

--- src/SniffCodesCommand.php
+++ src/SniffCodesCommand.php
@@ -5,13 +5,25 @@ namespace Kodosunifa;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Process\Process;
 
 class SniffCodesCommand extends Command
 {
+    use SniffHelper;
+
+    const STANDARD_CODE = 'PSR2';
+
+    /**
+     * List of directory names to not sniff
+     *
+     * @var array
+     */
+    protected $invalidDirectories = ['test', 'tests', 'storage', 'build', 'vendor', 'public', 'resources'];
+
     protected function configure()
     {
-        $this->setName('kodosunifa:sniff-code')->setDescription('Sniff code into psr-2 based');
+        $this->setName('kodosunifa:sniff')->setDescription('Sniff code into psr-2 based');
     }
 
     /**
@@ -22,35 +34,79 @@ class SniffCodesCommand extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $output->writeln('Sniffing Codes...');
+        $io = new SymfonyStyle($input, $output);
+        $directoriesToTest = $this->getDirectoriesToTest();
+        $gitCurrentBranch = $this->getCurrentBranch();
 
-        $process = new Process('phpcs -n --standard=PSR2 --ignore=*/tests/*,*/data/*,*/storage/*,*/test/*,*/vendor/* app/ bootstrap/ config, routes');
+        $this->setSnifferInformation($io, [
+                'STANDARD: ' => self::STANDARD_CODE,
+                'DIRECTORIES: ' => $directoriesToTest,
+                'GIT BRANCH: ' => $gitCurrentBranch
+            ]
+        );
+
+        $process = new Process('phpcs -n --standard=' . self::STANDARD_CODE . ' ' . $directoriesToTest);
         $process->run();
+        $sniffResult = $process->getOutput();
 
-        if ($process->getOutput()) {
-            $this->handleErrorMessages($process->getOutput(), $output);
+        if ($sniffResult) {
+            $io->text($sniffResult);
+            $this->handleMessages($io, 'error', 'STYLE CHECK PLEASE FIX BEFORE PROCEEDING!');
             exit(1);
         }
 
-        $this->handleSuccessMessages($output);
+        $this->handleMessages($io, 'success', sprintf('STYLE CHECK PASSED PUSHING TO BRANCH %s', $gitCurrentBranch));
         exit(0);
     }
 
     /**
-     * @param string $message
-     * @param OutputInterface $output
+     * @return string
      */
-    private function handleErrorMessages($message, OutputInterface $output)
+    private function getCurrentBranch()
     {
-        $output->writeln($message);
-        $output->writeln('<error>STYLE CHECK FOUND ERRORS PLEASE FIX BEFORE PROCEEDING!</error>');
+        $branch = new Process('git rev-parse --abbrev-ref HEAD');
+        $branch->run();
+
+        return $branch->getOutput();
     }
 
     /**
-     * @param OutputInterface $output
+     * Helper method to get directories needed to tes
+     * @return string
      */
-    private function handleSuccessMessages(OutputInterface $output)
+    private function getDirectoriesToTest()
     {
-        $output->writeln('<info>PSR-2 STYLE CHECK PASSED</info>');
+        return $this->removeInvalidDirAndImplodeValidDir($this->getApplicationDirectories());
+    }
+
+    /**
+     * Removes not needed directories based on the invalidDirectories list
+     *
+     * @param $collectedDirectoryBaseName
+     * @return string
+     */
+    private function removeInvalidDirAndImplodeValidDir($collectedDirectoryBaseName)
+    {
+        $validDirectories = array_diff($collectedDirectoryBaseName, $this->invalidDirectories);
+        $implodedDirectories = implode('/ ', $validDirectories);
+
+        return $implodedDirectories . '/';
+    }
+
+    /**
+     * Gest all directories(only) from the app root
+     *
+     * @return array
+     */
+    private function getApplicationDirectories()
+    {
+        $collectedDirectoryBaseName = [];
+        $directories = glob(__DIR__ . '/../../../../*', GLOB_ONLYDIR);
+
+        foreach ($directories as $directory) {
+            $collectedDirectoryBaseName[] = basename($directory);
+        }
+
+        return $collectedDirectoryBaseName;
     }
 }
